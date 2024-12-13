c056d641141d4ef2fe20cab517e864be2f47162a
Rolando_cf <rolandodj83@gmail.com>
2024-12-13 20:52:36 +0100

    Revisar porque los campos INPUT se escriben a la vez

--- src/component/RegistrationForm.js
+++ src/component/RegistrationForm.js
@@ -2,37 +2,35 @@ import React, { useState } from "react";
 
 const InputText = () => {
     const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
 
-    const handleChange = (event) => {
+    const handleChangeName = (event) => {
         setName(event.target.value);
     }
 
-    return(
+    const handleChangeEmail = (event) => {
+        setEmail(event.target.value)
+    };
+
+    // Aquí puedes manejar el envío del formulario 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Formulario enviado:", { name, email });
+    };
+
+
+    return (
         <div>
             <form>
-            <input type="text" value={name} onChange={handleChange}></input>
-            <button>Enviar</button>
-            <p>Nombre: {name}</p>
+                <input type="text" value={name} onChange={handleChangeName}></input>
+                <input type="email" value={email} onChange={handleChangeEmail}></input>
+                <button type="submit">Enviar</button>
+                <p>Nombre: {name}</p>
+                <p>Email: {email}</p>
             </form>
         </div>
     );
 };
 
-const InputEmail = () => {
-    const [email, setEmail] = useState("");
-
-    const handleChange = (event) => {
-        setEmail(event.target.value);
-    }
-
-    return(
-        <div>
-            <form>
-                <input type="email" value={email} onChange={handleChange}></input>
-                <p>Email: {email}</p>
-            </form>
-        </div>
-    );
-}
 
 export default InputText;
